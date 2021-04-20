ba3907a84d66f2d182a1ea8377f41e600392daf5
karamalie <karam7898@gmail.com>
2021-04-20 09:02:41 +0800

    chore(package.json): semantic release config
    CI/CD setup

--- src/main.js
+++ src/main.js
@@ -104,6 +104,25 @@ export class LottieInteractivity {
     this.#cursorHandler(-1, -1);
   };
 
+  animate({ timing, draw, duration }) {
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+      // timeFraction goes from 0 to 1
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+
+      // calculate the current animation state
+      let progress = timing(timeFraction);
+
+      draw(progress); // draw it
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  }
+
   #cursorHandler = (x, y) => {
     // Resolve cursor position if cursor is inside container
     if (x !== -1 && y !== -1) {
